6992b1040df859635935c9089852f863a7b3236a
its-a-feature <codybthomas@gmail.com>
2023-04-10 11:36:55 -0500

    bumping the pypi version

--- mythic_container/__init__.py
+++ mythic_container/__init__.py
@@ -3,7 +3,7 @@ from .mythic_service import start_and_run_forever, test_command
 
 containerVersion = "v1.0.0-0.0.7"
 
-PyPi_version = "0.2.0-rc14"
+PyPi_version = "0.2.0-rc17"
 
 RabbitmqConnection = rabbitmqConnectionClass()
 
